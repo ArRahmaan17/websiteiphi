81147fc16769d37a92f106e761dcb97b780e4bda
ArRahmaan17 <ardrah17@gmail.com>
2021-12-17 21:13:30 +0700

    update

--- app/Views/pages/detailkegiatan.php
+++ app/Views/pages/detailkegiatan.php
@@ -3,7 +3,7 @@
 <div class="container mb-5" data-aos="fade-up" data-aos-delay="1000">
   <p class="h3 text-capitalize text-light">Acara IPHI <?= $data['judul'] ?></p>
   <div class="card">
-    <img src="/img/<?= $data['foto'] ?>" class="img-fluid">
+    <iframe width="560" height="315" src="" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     <div class="card-body">
       <p class="h3">Diposting Oleh: <?= $data['penulis'] ?> </p>
       <p class="h5">Terakhir Diupdate: <?= $data['updated_at'] ?></p>
--- app/Views/pages/index.php
+++ app/Views/pages/index.php
@@ -3,21 +3,21 @@
 <div class="container">
   <div class="row">
     <div class="g-col-12 text-center text-light">
-      <img src="img/ketua.webp" data-aos="zoom-in-down" data-aos-delay="500" class=" img rounded-circle" width="250">
+      <img src="img/ketua.webp" data-aos="zoom-in-down" data-aos-delay="500" class="img rounded-circle shadow" width="250">
       <p class="h1" data-aos="fade-right" data-aos-delay="550">H. Juliyatmono, MM</p>
       <p class="h5" data-aos="fade-right" data-aos-delay="600">Ketua IPHI Kabupaten KARANGANYAR</p>
     </div>
   </div>
   <div id="fisimisi" class="row">
     <div class="col-sm-12 col-xl-6 mt-1" data-aos="fade-up" data-aos-delay="1000">
-      <div class="card">
+      <div class="card shadow">
         <div class="card-body">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit odio sapiente sequi ducimus omnis necessitatibus at illum minus laudantium, vel eaque nostrum unde, eligendi inventore quibusdam voluptatum, provident quasi culpa!
         </div>
       </div>
     </div>
     <div class="col-sm-12 col-xl-6 mt-1" data-aos="fade-up" data-aos-delay="1000">
-      <div class="card">
+      <div class="card shadow">
         <div class="card-body">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit odio sapiente sequi ducimus omnis necessitatibus at illum minus laudantium, vel eaque nostrum unde, eligendi inventore quibusdam voluptatum, provident quasi culpa!
         </div>
@@ -27,7 +27,7 @@
   <div id="acara" class="row p-4">
     <p class="h1 text-light" data-aos="fade-down" data-aos-delay="100">Acara IPHI & Panti</p>
     <div class="col-xl-4 mb-3" data-aos="fade-up" data-aos-delay="150">
-      <div class="card">
+      <div class="card shadow">
         <img src="/img/default.webp" class="card-img-top">
         <div class="card-body">
           <h5 class="card-title">Judul Postingan</h5>
@@ -37,7 +37,7 @@
       </div>
     </div>
     <div class="col-xl-4 mb-3" data-aos="fade-up" data-aos-delay="150">
-      <div class="card">
+      <div class="card shadow">
         <img src="/img/default.webp" class="card-img-top">
         <div class="card-body">
           <h5 class="card-title">Judul Postingan</h5>
@@ -47,7 +47,7 @@
       </div>
     </div>
     <div class="col-xl-4 mb-3" data-aos="fade-up" data-aos-delay="150">
-      <div class="card">
+      <div class="card shadow">
         <img src="/img/default.webp" class="card-img-top">
         <div class="card-body">
           <h5 class="card-title">Judul Postingan</h5>
@@ -57,5 +57,26 @@
       </div>
     </div>
   </div>
+  <hr>
+  <!-- Gallery -->
+  <div id="gallery" class="row" data-aos="slide-up" data-aos-delay="500">
+    <p class="h3 text-center text-light">OUR GALLERY</p>
+    <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+      <img src="/img/default.webp" class="w-100 shadow rounded mb-4" alt="Boat on Calm Water" />
+      <img src="/img/default.webp" class="w-100 shadow rounded mb-4" alt="Wintry Mountain Landscape" />
+      <img src="/img/default.webp" class="w-100 shadow rounded mb-4" alt="Wintry Mountain Landscape" />
+    </div>
+
+    <div class="col-lg-4 mb-4 mb-lg-0">
+      <img src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.jpg" class="w-100 shadow rounded mb-4" alt="Mountains in the Clouds" />
+      <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).jpg" class="w-100 shadow rounded mb-4" alt="Boat on Calm Water" />
+    </div>
+
+    <div class="col-lg-4 mb-4 mb-lg-0">
+      <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).jpg" class="w-100 shadow rounded mb-4" alt="Waves at Sea" />
+      <img src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.jpg" class="w-100 shadow rounded mb-4" alt="Yosemite National Park" />
+    </div>
+  </div>
+  <!-- Gallery -->
 </div>
 <?= $this->endSection(); ?>
